b02462b58935bb18d5333407be16da03b0b0e4b4
“Rohit <rohit@virtualsamaj.com>
2018-04-04 20:47:24 -0700

    Diagnostic and Prognostics Algorithms: Original implementation by Dr Eric Bechhoefer, Chief Engineer, NRG Systems.

---
+++ Analyses/bearingScript.m
@@ -0,0 +1,28 @@
+function bearingScript(bearing)
+
+
+[rd,pd,ca,ne,side] = NiceBearing;
+
+faultRates = ones(5,1); %[cage, ball, outer, inner, shaft];
+for i = 1:4,
+    faultRates(i) = GetBearFreqRatio(rd,pd,ca,ne,i,side);
+end
+faultFreq = faultRates * bearing.rate;
+
+ [env,dty] = envelope1(bearing.gs,1/bearing.sr,128,2000,4000);
+ 
+ [spec, freq] = psde(env, 2048,1/dty, 1024);
+ 
+ plot(freq,spec);
+ ax = axis();
+ hold on
+ for i = 1:5,
+     f = faultFreq(i);
+     plot([f f],ax(3:4),'LineWidth',2)
+ end
+ hold off
+ legend('env','cage','ball','outer','inner','1/rev')
+ xlabel('Hz')
+ ylabel('Gs')
+ axis([0 200 ax(3:4)])
+ 
