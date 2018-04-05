7f7d0a601b8c3fbba44a6ce30667199037e71c5e
“Rohit <rohit@virtualsamaj.com>
2018-04-04 23:05:48 -0700

    Reformatted code

--- Analyses/NiceBearing.m
+++ Analyses/NiceBearing.m
@@ -1,16 +1,17 @@
-function [rd,pd,ca,ne,side] = NiceBearing
+function [rd, pd, ca, ne, side] = NiceBearing
+%NICEBEARING Summary of this function goes here
+%   rd:     roller diameter
+%   pd:     pitch diameter
+%   ca:     contact angle in degrees
+%   ne:     number of elements
+%   type    enum[cage, roller, outer race, inner race]
+%   side:   inner or outer race fixed.  inner = 1, outer = 2;
+%   Eric Bechhoefer, April 10, 2009 for PHM Conference
 
-%rd:    roller diameter
-%pd:    pitch diameter
-%ca:    contact angle in degrees
-%ne:    number of elements
-%type   enum[cage, roller, outer race, inner race]
-%side: inner or outer race fixed.  inner = 1, outer = 2;
-%Eric Bechhoefer, April 10, 2009 for PHM Conference
-rd = .235;
-ca = 0;
-ne = 8;
-pd = 1.245;
+    rd = .235;
+    ca = 0;
+    ne = 8;
+    pd = 1.245;
 
-side = 2;
-
+    side = 2;
+end
