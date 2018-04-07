d9bfbc68e17106248fe5adf3e9a2562dadf8bc24
“Rohit <rohit@virtualsamaj.com>
2018-04-07 00:02:26 -0700

    Use of xlim and ylim instead of axis as these are used in the App

--- Analyses/bearingScript.m
+++ Analyses/bearingScript.m
@@ -2,29 +2,31 @@ function bearingScript(bearing)
 %BEARINGSCRIPT Summary of this function goes here
 %   Detailed explanation goes here
 
-    [rd,pd,ca,ne,side] = NiceBearing;
+    [rd, pd, ca, ne, side] = NiceBearing;
 
-    faultRates = ones(5,1); %[cage, ball, outer, inner, shaft];
+    faultRates = ones(5, 1); %[cage, ball, outer, inner, shaft];
     for i = 1:4
-        faultRates(i) = GetBearFreqRatio(rd,pd,ca,ne,i,side);
+        faultRates(i) = GetBearFreqRatio(rd, pd, ca, ne, i, side);
     end
 
     faultFreq = faultRates * bearing.rate;
 
-    [env,dty] = envelope1(bearing.gs,1/bearing.sr,128,2000,4000);
+    [env, dty] = envelope1(bearing.gs, 1/bearing.sr, 128, 2000, 4000);
 
 	[spec, freq] = psde(env, 2048,1/dty, 1024);
 
-    plot(freq,spec);
-    ax = axis();
+    plot(freq, spec);
+    ax = [xlim ylim]; % axis();
     hold on
     for i = 1:5
         f = faultFreq(i);
-        plot([f f],ax(3:4),'LineWidth',2)
+        plot([f f], ax(3:4),'LineWidth',2)
     end
     hold off
     legend('env','cage','ball','outer','inner','1/rev')
     xlabel('Hz')
     ylabel('Gs')
-    axis([0 200 ax(3:4)])
+    %axis([0 200 ax(3:4)])
+    xlim([0 200])
+    ylim(ax(3:4))
 end
